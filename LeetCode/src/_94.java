e70ea260441af76ebc6222ba2b6f588740755be0
Xianren-Zhang <599893448@qq.com>
2021-10-12 13:19:17 +0800

    update

--- LeetCode/src/_94.java
+++ LeetCode/src/_94.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class _94 {
@@ -8,6 +10,8 @@ public class _94 {
 }
 
 class Solution_94 {
+
+    //region 递归
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         traversal(root, list);
@@ -23,4 +27,35 @@ class Solution_94 {
         list.add(root.val);
         traversal(root.right, list);
     }
+    //endregion
+
+    //region 迭代
+    public List<Integer> inorderTraversal_iteratively(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Deque<TreeNode> deque = new LinkedList<>();
+
+        if (root == null) {
+            return list;
+        }
+
+        TreeNode cur = root;
+        while (!deque.isEmpty() || cur != null) {
+            //尽可能将该节点的左子树压栈
+            while (cur != null) {
+                deque.push(cur);
+                cur = cur.left;
+            }
+
+            TreeNode node = deque.pop();
+            list.add(node.val);
+            //如果该节点有右子树，切换到右子树
+            if (node.right != null) {
+                cur = node.right;
+            }
+        }
+
+        return list;
+    }
+    //endregion
+
 }
