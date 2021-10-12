09aa6fc45479b25c6fb10b4a14e64670cc74ce55
Xianren-Zhang <599893448@qq.com>
2021-10-12 13:31:19 +0800

    update

--- LeetCode/src/_145.java
+++ LeetCode/src/_145.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class _145 {
     public static void main(String[] args) {
@@ -8,6 +7,8 @@ public class _145 {
 }
 
 class Solution_145 {
+
+    //region 递归
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         traversal(root, list);
@@ -23,5 +24,33 @@ class Solution_145 {
         traversal(root.right, list);
         list.add(root.val);
     }
+    //endregion
+
+    //region 迭代
+    public List<Integer> postorderTraversal_iteratively(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Deque<TreeNode> deque = new LinkedList<>();
+
+        if (root == null) {
+            return list;
+        }
+
+        deque.push(root);
+        while (!deque.isEmpty()) {
+            TreeNode node = deque.pop();
+            list.add(node.val);
+            if (node.left != null) {
+                deque.push(node.left);
+            }
+            if (node.right != null) {
+                deque.push(node.right);
+            }
+        }
+
+        Collections.reverse(list);
+        return list;
+    }
+    //endregion
+
 }
 
